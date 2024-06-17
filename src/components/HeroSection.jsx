4537e9fcb2a38ebad015bb0673b8cbd94dc955d5
wassim mezghanni <ge27tuv@mytum.de>
2024-06-17 20:23:37 +0200

    profile picture modification

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -64,7 +64,7 @@ const HeroSection = () => {
       <div className="col-span-5 place-self-center mt-4 lg:mt-0">
         <div className="bg-[#181818] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative">
           <Image
-            src="/images/hero_image.png" //Wassim Mezghanni.jpg
+            src="/images/profile-pic.png" //Wassim Mezghanni.jpg
             alt="Wassim Mezghanni's portrait"
             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             width={300}
